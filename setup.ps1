77aa043ab4e6a28d79a9f67a99a8926f5dba0741
Sergio <gabrielsanv97@gmail.com>
2023-11-30 16:47:59 -0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -32,3 +32,6 @@ nvm use 16
 winget install Microsoft.DotNet.SDK.6
 
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+
+pyenv install 3.10.5
+pyenv global 3.10.5
