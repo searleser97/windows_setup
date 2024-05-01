aa158c8e1c7028e4a05edecd970fa8a2a61d97ba
Sergio <gabrielsanv97@gmail.com>
2024-05-01 16:26:46 -0700

    Update gitrebasemain.ps1

--- gitrebasemain.ps1
+++ gitrebasemain.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 $initialBranch = $(git branch --show-current)
 $baseBranch = "users/sergiosanc/localConfig"
 git fetch origin main:main
-git switch $baseBranch
+git switch --ignore-other-worktrees $baseBranch
 git pull
 git rebase origin/main
 git switch $initialBranch
