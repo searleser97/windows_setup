0dfd0a1011834cdb634d7226ea7ec457fca80e64
Sergio <gabrielsanv97@gmail.com>
2024-05-03 15:26:38 -0700

    Update gitlogsince.ps1

--- gitlogsince.ps1
+++ gitlogsince.ps1
@@ -1,6 +1,15 @@
 param(
     [Parameter(Mandatory = $true)]
-    [string] $Reference
+    [string] $From,
+    [Parameter(Mandatory = $false)]
+    [string] $To
 )
 
-git rev-list "$Reference~1..HEAD" --no-commit-header --pretty="%Cblue%h%Creset (%ch by %Cgreen%an%Creset) %s"
+if (! $To) {
+  $To = "HEAD"
+}
+
+$FromDate=git show --no-patch --format=%ci $From
+$ToDate=git show --no-patch --format=%ci $To
+
+git log --since="$FromDate" --until="$ToDate" --pretty="%Cblue%h%Creset (%ch by %Cgreen%an%Creset) %s"
