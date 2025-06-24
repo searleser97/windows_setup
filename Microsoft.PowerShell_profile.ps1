2b4dfdf4902f8c833c8530391013887b70db356d
Sergio <gabrielsanv97@gmail.com>
2025-06-24 10:34:27 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,14 @@
 # Set-Alias -Name x86_64-w64-mingw32-gcc -Value gcc -Scope Global
-Set-PSReadLineKeyHandler -Chord "Tab" -Function ForwardWord
+# Set-PSReadLineKeyHandler -Chord "Tab" -Function ForwardWord
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 $env:FZF_DEFAULT_COMMAND='fd --type d -i'
-cd E:\
+
+if ($PWD.Path -eq $HOME -and $args.Count -eq 0 -and -not $Host.CurrentCommandParameter) {
+    cd E:\
+}
+
 function sd {
     $dir = $(fzf)
     if ($dir) {
