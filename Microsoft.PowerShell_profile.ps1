8b809d290d959a41cd3378214f0e839de20fa732
Sergio <gabrielsanv97@gmail.com>
2025-06-24 09:33:04 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,13 @@
-Set-Alias -Name x86_64-w64-mingw32-gcc -Value gcc -Scope Global
+# Set-Alias -Name x86_64-w64-mingw32-gcc -Value gcc -Scope Global
 Set-PSReadLineKeyHandler -Chord "Tab" -Function ForwardWord
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-$env:FZF_DEFAULT_COMMAND='fd --type d'
-
+$env:FZF_DEFAULT_COMMAND='fd --type d -i'
+cd E:\
 function sd {
-    cd $(fzf)
+    $dir = $(fzf)
+    if ($dir) {
+        cd $dir
+    }
 }
-
-Set-Alias gitlogsince $HOME\windows_setup\gitlogsince.ps1
-Set-Alias gitbranchfrom $HOME\windows_setup\gitbranchfrom.ps1
-Set-Alias gitlogauthor $HOME\windows_setup\gitlogauthor.ps1
-Set-Alias gitrebasemain $HOME\windows_setup\gitrebasemain.ps1
-Set-Alias gitpush $HOME\windows_setup\gitpush.ps1
-Set-Alias gitlogtree $HOME\windows_setup\gitlogtree.ps1
