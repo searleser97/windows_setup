1ff04b4ce16a8627d59f8ff2f8d4a2f4981dbda3
Sergio <gabrielsanv97@gmail.com>
2025-06-25 09:49:50 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,15 +3,24 @@
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-$env:FZF_DEFAULT_COMMAND='fd --type d -i'
 
 if ($PWD.Path -eq $HOME -and $args.Count -eq 0 -and -not $Host.CurrentCommandParameter) {
     cd E:\
 }
 
 function sd {
+    $env:FZF_DEFAULT_COMMAND='fd --type d -i -H -d 13'
     $dir = $(fzf)
     if ($dir) {
+        cd $dir
+    }
+}
+
+function sf {
+    $env:FZF_DEFAULT_COMMAND='fd --type f -i -H -d 13'
+    $file = $(fzf)
+    if ($file) {
+        $dir = Split-Path -Parent $file
         cd $dir
     }
 }
